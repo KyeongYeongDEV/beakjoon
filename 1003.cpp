ed6d3ac217741e66a6978313dc014532ee5a109e
KyeongYeongDEV <yeong7098@naver.com>
2022-01-30 23:43:37 +0900

    2022.01.30

--- 1003.cpp
+++ 1003.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 //처음에는 그냥 재귀로 풀었는데 시간초과가 나왔다
-//찾아보니 DP(다이나믹 프로그래밍)으로 풀어야 한다더라 
+//찾아보니 DP(다이나믹 프로그래밍) 메모이제이션으로 풀어야 한다더라 
 //DP 공부를 해봐야 할 것 같다
 
 int dp[41]={0};
@@ -26,7 +26,7 @@ int main(){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
         int a;
         cin >> a;
         
-        if( a == 0){ cout << "1 0\n";}
+        if(a == 0){ cout << "1 0\n";}
         else if(a ==1){cout << "0 1\n";}
         else{
             fibo(a);
