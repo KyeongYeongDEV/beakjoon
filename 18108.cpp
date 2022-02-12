d1fe1c719a24528aa9f7bf068fa27cebfb2cc54a
KyeongYeongDEV <yeong7098@naver.com>
2022-02-12 17:04:20 +0900

    2022.02.12

--- 18108.cpp
+++ 18108.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main(){cin.tie(0); cout.tie(0);
+int main(){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     int num;
     cin >> num;
     cout << num-543;
