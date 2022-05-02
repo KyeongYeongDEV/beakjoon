70892adfd19335fe6c44869ca23665c572922540
KyeongYeongDEV <yeong7098@naver.com>
2022-05-02 20:35:12 +0900

    2022.05.02

--- 1012.cpp
+++ 1012.cpp
@@ -1,74 +1,67 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
+#include<algorithm>
 using namespace std;
 
-int M,N,K;
-int arr[51][51] = {0,};
-bool check[51][51]={false,};
-int dx[] = {1,0,-1,0};
-int dy[] = {0,1,0,-1};
-queue<pair<int, int>> q;
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, -1, 0, 1 };
 
-void bfs(int x,int y){
-    q.push(make_pair(x,y));
+queue<pair<int, int> > q;
+int N, M, K;
+int cabbage[50][50];
+bool visited[50][50];
 
-    while(!q.empty()){
-        int a = q.front().first; // x
-        int b = q.front().second;// y
+void bfs(int xx, int yy) {
+	q.push( make_pair(xx, yy) );
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		visited[x][y] = true;
 
-        q.pop();
-        check[a][b]=true;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 
-        for(int i =0; i < 4; i++){
-            int nx = a + dx[i];
-            int ny = b + dy[i];
-
-            if(0 <= nx && 0 <= ny && nx <N && ny < M &&
-                !check[nx][ny] && arr[nx][ny] ==1){
-                q.push({ nx,ny });
-                check[nx][ny] = true;
-            }
-        }
-    }
+			if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && cabbage[nx][ny] == 1) {
+				visited[nx][ny] = true;
+				q.push( make_pair(nx, ny) );
+			}
+		}
+	}
 }
-
-void reset(){
-    while(!q.empty()){ //큐가 빌 때까지 
-        q.pop();
-    }
-    for(int i =0; i < N;i ++){
-        for(int j =0 ; i <M; j++){
-            check[i][j] = false;
-            arr[i][j] = 0;
-        }
-    }
+void init() {
+	while (!q.empty()) q.pop();
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			visited[i][j] = false;
+			cabbage[i][j] = 0;
+		}
+	}
 }
+int main(void) {
+	int T;
+	cin >> T;
 
-int main(){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
-    int T;
-    cin>> T;
+	for (int t = 0; t < T; t++) {
+		init();
 
-    for(int t = 0; t< T; t++){
-        int cnt =0;
-        reset();
+		cin >> M >> N >> K;
+		int cnt = 0;
 
-        cin >> M>>N>>K; //가로, 세로, 배추의 수
-
-        for(int i =0; i< K; i++){
-            int x,y;
-            cin >>x>>y;
-            arr[x][y] = 1; //배추가 심어져 있는 곳에 1
-        }
-
-        for(int i = 0; i <N; i++){
-            for(int j = 0; j < M; j++){
-                if(arr[i][j] ==1 && !check[i][j]){
-                    bfs(i,j);
-                    cnt++;
-                }
-            }
-        }
-
-        cout << cnt<<"\n";
-    }
+		for (int j = 0; j < K; j++) {
+			int x, y;
+			cin >> y >> x;
+			cabbage[x][y] = 1;
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (cabbage[i][j] == 1 && !visited[i][j]) {
+					bfs(i, j);
+					cnt++;
+				}
+			}
+		}
+		cout << cnt << endl;
+	}
 }
