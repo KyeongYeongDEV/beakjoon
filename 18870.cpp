a26bfab9fc8f23fdff34bcf1ab59d97857991391
KyeongYeongDEV <yeong7098@naver.com>
2022-07-05 17:23:45 +0900

    Done

--- 18870.cpp
+++ 18870.cpp
@@ -15,7 +15,9 @@ int main(){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     vector<int> cpy_vec(vec);
     sort(cpy_vec.begin(),cpy_vec.end());
     cpy_vec.erase(unique(cpy_vec.begin(),cpy_vec.end()),cpy_vec.end());
-
+    //unique함수 
+    // 정렬된 1234455 를 1234545로 바꿔준 후 뒤에 있는 45의 4부분의 주소를 반환한다.
+    //erase를 이용해 unique로부터 얻은 주소부터 끝까지 지워주면 된다.
     for(int i=0; i < num; i++);
     for(int i=0; i< num; i++){
         auto it = lower_bound(cpy_vec.begin(),cpy_vec.end(),vec[i]);
